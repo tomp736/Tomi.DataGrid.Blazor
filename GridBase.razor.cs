e673aba29f293c71ca35d14837e1352b750417ae
Thomas Pisula <tompisula@gmail.com>
2021-02-09 11:40:54 +0100

    Add project files.

---
+++ GridBase.razor.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tomi.DataGrid.Blazor
+{
+    public class GridBase<TableItem, Item> : ComponentBase, IDisposable
+        where TableItem : GridItem<Item>
+    {
+        [Parameter]
+        public string GridTitle { get; set; }
+
+        [Parameter]
+        public RenderFragment GridHeader { get; set; }
+
+        [Parameter]
+        public RenderFragment<TableItem> GridRow { get; set; }
+
+        [Parameter]
+        public RenderFragment<TableItem> EditRow { get; set; }
+
+        [Parameter]
+        public IEnumerable<Item> Items { get; set; }
+
+        public List<GridItem<Item>> GridItems { get; private set; } = new List<GridItem<Item>>();
+
+        protected override void OnInitialized()
+        {
+            Console.WriteLine(Items.Count());
+            GridItems.AddRange(Items.Select(n => new GridItem<Item>(n)));
+        }
+
+        public void RemoveItem(Item item)
+        {
+            GridItems.RemoveAll(n => n.Item.Equals(item));
+        }
+
+        public void AddItem(Item item, bool isEditing)
+        {
+            GridItems.Insert(0, new GridItem<Item>(item) { Editing = isEditing });
+        }
+
+        public void SetEditMode(Item item, bool isEditing)
+        {
+            foreach (var gItem in GridItems)
+            {
+                if (gItem.Item.Equals(item))
+                {
+                    gItem.Editing = isEditing;
+                }
+            }
+        }
+
+        public void Update()
+        {
+            GridItems.Clear();
+            GridItems.AddRange(Items.Select(n => new GridItem<Item>(n)).ToList());
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ GridItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Tomi.DataGrid.Blazor
+{
+    public class GridItem<T>
+    {
+        public bool Selected { get; set; }
+        public bool Editing { get; set; }
+        public bool ForceTop { get; set; }
+        public T Item { get; set; }
+
+        public GridItem(T item)
+        {
+            Item = item;
+        }
+
+        public override string ToString()
+        {
+            return Item.ToString() + $"Editing: {Editing}";
+        }
+    }
+}
